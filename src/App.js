be8cb97b3fc16325dc73d6017eaeaf178ea40bda
Thom-Ri <mune.thom2@gmail.com>
2021-07-19 20:28:02 -0300

    modificador

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
@@ -11,9 +12,24 @@ const HolaApp = () => {
 
 
 function App() {
+  const [state, setstate] = useState(0)
+
+  function sumar() {
+    setstate(state + 1)
+  }
+
+  function restar() {
+    if(state != 0){
+      setstate(state - 1)
+    }
+  }
+
   return (
     <div className="App">
       <NavBar/>
+      <button onClick={restar}>-</button>
+      {state}
+      <button onClick={sumar}> + </button>
     </div>
   );
 }
