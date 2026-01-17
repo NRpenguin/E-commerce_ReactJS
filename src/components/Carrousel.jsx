7b991a2e0c61ea9845c64dab4418507e54081ce1
NRpenguin <79615117+NRpenguin@users.noreply.github.com>
2026-01-16 22:01:07 -0300

    Update carousel image sources to new filenames

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -6,13 +6,13 @@ function Carrousel() {
                     <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
                         <div className="carousel-inner">
                             <div className="carousel-item active">
-                                <img src="../assets/Img/carrousel/1.jpg" className="d-block w-100" alt="..." height="700px" />
+                                <img src="../assets/Img/carrousel/car1.jpg" className="d-block w-100" alt="..." height="700px" />
                             </div>
                             <div className="carousel-item">
-                                <img src="../assets/Img/carrousel/2.jpg" className="d-block w-100" alt="..."  height="700px" />
+                                <img src="../assets/Img/carrousel/car2.jpg" className="d-block w-100" alt="..."  height="700px" />
                             </div>
                             <div className="carousel-item">
-                                <img src="../assets/Img/carrousel/3.jpg" className="d-block w-100" alt="..."  height="700px" />
+                                <img src="../assets/Img/carrousel/car3.jpg" className="d-block w-100" alt="..."  height="700px" />
                             </div>
                         </div>
                         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
